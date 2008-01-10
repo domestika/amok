38a6ae779145ec12f04f351ed10bfcdd971b7262
mbc <mbc@c6609649-f813-0410-a02d-d8aefb0e6eb3>
2008-01-10 14:37:19 +0000

    First import
    git-svn-id: http://svn.devactiva.com/webpop-core/amok/trunk@5406 c6609649-f813-0410-a02d-d8aefb0e6eb3

---
+++ Amok.php
@@ -0,0 +1,129 @@
+<?php
+
+class Amok
+{
+  private $_name, $_expectations;
+  
+  public function __construct($name = 'Mock')
+  {
+    $this->_name = $name;
+    $this->_expectations = array();
+  }
+  
+  public function expects($function) {
+    $expectation = new AmokExpectation($function);
+    $this->_expectations[] =  $expectation;
+    return $expectation;
+  }
+  
+  public function verify()
+  {
+    $no_matches = array();
+    foreach($this->_expectations as $expectation) {
+      if(!$expectation->hasBeenMatched()) {
+        $no_matches[] = $expectation;
+      }
+    }
+    if(empty($no_matches)) {
+      return true;
+    }
+    $error = "";
+    foreach($no_matches as $expectation) {
+      $error .= "Mock {$this->_name} expected {$expectation->get_function()} with arguments: ". print_r($expectation->get_arguments(), true). "\n";
+    }
+    throw new AmokNoMatchException($error);
+  }
+  
+  function __call($function, $arguments) {
+    foreach($this->_expectations as $expectation) {
+      if($expectation->checkMatch($function,$arguments)) {
+        return $expectation->execute();
+      }
+    }
+    throw new AmokNoMatchException("No match for method $function with args: ". print_r($arguments, true));
+  }
+}
+
+class AmokExpectation 
+{
+  private $_function, $_arguments, $_arg_hash, $_times, $_number_of_calls, $_return_value, $_raises, $_matched;
+  
+  function __construct($function) {
+    $this->_function = $function;
+    $this->_times = 1;
+    $this->_number_of_calls = 0;
+    $this->_raises = false;
+    $this->_matched = false;
+  }
+  
+  public function get_function()
+  {
+    return $this->_function;
+  }
+  
+  public function get_arguments()
+  {
+    return $this->_arguments;
+  }
+  
+  public function with($args) {
+    $this->arguments = func_get_args();
+    $this->_arg_hash = md5(print_r(func_get_args(),true));
+    return $this;
+  }
+  
+  public function times($number) {
+    $this->_times = $number;
+    if($number <= 0) {
+      $this->_matched = true;
+    }
+    return $this;
+  }
+  
+  public function returns($value)
+  {
+    $this->_return_value = $value;
+    return $this;
+  }
+  
+  public function raises($exception)
+  {
+    $this->_raises = true;
+    $this->_return_value = $exception;
+    return $this;
+  }
+
+  public function execute() {
+    if($this->_raises) {
+      throw $this->_return_value;
+    }
+    return $this->_return_value;
+  }
+  
+  public function checkMatch($function, $args)
+  {
+    if($this->_matched) {
+      return false;
+    }
+    if($this->_function == $function && $this->_arg_hash == md5(print_r($args,true))) {
+      $this->_number_of_calls++;
+      if($this->_number_of_calls == $this->_times) {
+        $this->_matched = true;
+      } else if($this->_number_of_calls > $this->_times) {
+        $this->_matched = false;
+        throw new AmokNoMatchException("Function $function with args: ". print_r($args,true) ."\n Was called {$this->_number_of_calls} times, only {$this->_times} calls expected");
+      }
+      return true;
+    }
+    return false;
+  }
+  
+  public function hasBeenMatched()
+  {
+    return $this->_matched;
+  }
+}
+
+class AmokNoMatchException extends Exception {}
+
+?>
---
+++ AmokTest.php
@@ -0,0 +1,73 @@
+<?php
+require 'Amok.php';
+
+class AmokTest extends PHPUnit_Framework_TestCase
+{
+  public function test_mock_with_2_calls_to_some_call()
+  {
+    $my_mock = new Amok('Thingy');
+    $my_mock->expects('some_call')->times(2)->with(24)->returns(200);
+    
+    $this->assertEquals($my_mock->some_call(24),200);
+    $this->assertEquals($my_mock->some_call(24),200);
+    $this->assertTrue($my_mock->verify());
+  }
+  
+  public function test_mock_with_too_many_calls_to_some_call()
+  {
+    $my_mock = new Amok('Thingy');
+    $my_mock->expects('some_call')->times(2)->with(24)->returns(200);
+    
+    $this->assertEquals($my_mock->some_call(24),200);
+    $this->assertEquals($my_mock->some_call(24),200);
+    
+    try {
+      $my_mock->some_call(24);    
+      $this->fail();
+    } catch(AmokNoMatchException $e) {
+      $this->assertTrue(true);
+    }
+    
+    $this->assertTrue($my_mock->verify());
+  }
+  
+  public function test_mock_with_no_calls_to_some_call()
+  {
+    $my_mock = new Amok('Thingy');
+    $my_mock->expects('some_call')->with(24)->returns(200);
+    
+    try {
+      $my_mock->verify();
+      $this->fail();
+    } catch(AmokNoMatchException $e) {
+      $this->assertTrue(true);
+    }
+  }
+  
+  public function test_mock_with_raises_set()
+  {
+    $my_mock = new Amok('Thingy');
+    $my_mock->expects('some_call')->with('23',array(1,2,3,4))->raises(new Exception('This is an error!'));
+    
+    try {
+      $my_mock->some_call('23',array(1,2,3,4));
+      $this->fail("some_call should raise an exception");
+    } catch(Exception $e) {
+      $this->assertEquals($e->getMessage(), 'This is an error!');
+    }
+  }
+  
+  public function test_mock_with_various_expectations_for_the_same_method()
+  {
+    $my_mock = new Amok('Thingy');
+    $my_mock->expects('some_call')->with('23')->returns(array(1,2,3,4));
+    $my_mock->expects('some_call')->with('24')->returns(array(5,6,7,8));
+    
+    $this->assertEquals($my_mock->some_call('24'),array(5,6,7,8));
+    $this->assertEquals($my_mock->some_call('23'),array(1,2,3,4));
+    
+    $this->assertTrue($my_mock->verify());
+  }
+}
+
+?>
